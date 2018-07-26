36212cce2ca8528cc535b882f002295d58466e4f
Ronnie Masamba <ronnie_masamba@hotmail.com>
2018-07-26 19:38:22 +0100

    New class models added

--- src/main/java/com/cooking/receipe/domain/Notes.java
+++ src/main/java/com/cooking/receipe/domain/Notes.java
@@ -1,4 +1,34 @@
 package com.cooking.receipe.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Lob
+    private String notes;
+
+
+    // private Receipe receipe;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
 }
--- src/main/java/com/cooking/receipe/domain/Receipe.java
+++ src/main/java/com/cooking/receipe/domain/Receipe.java
@@ -1,4 +1,97 @@
 package com.cooking.receipe.domain;
 
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Receipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String prepTime;
+    private String cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+   // @Enumerated(EnumType.STRING)
+   // private Difficulty difficulty;
+
+    // private Set<Ingredient> ingredient;
+
+    // private Notes notes;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public String getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(String cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
 }
+
